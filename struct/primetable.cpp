fcddd8fc3c32809e846de7e8a1db5d183047a1f9
allem40306 <allem40306@gmail.com>
2015-12-11 16:46:46 +0800

    init commit

---
+++ struct/primetable.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+#define N 20000000
+int ox[N], p[N], pr;
+
+void PrimeTable(){
+	ox[0] = ox[1] = 1;
+	pr = 0;
+	for (int i = 2; i < N; i++){
+		if (!ox[i]) p[pr++] = i;
+		for (int j = 0;i*p[j]<N&&j < pr; j++)
+			ox[i*p[j]] = 1;
+	}
+}
+
+int main(){
+	PrimeTable();
+}
