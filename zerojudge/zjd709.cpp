8394b10fe8534da527be17ac8616f6ac4a2111e4
allem40306 <allem40306@gmail.com>
2015-12-15 20:40:59 +0800

    init commit

---
+++ zerojudge/zjd709.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#define N 1000010
+int ox[N], p[N], pr;
+
+void PrimeTable() {
+	ox[0] = ox[1] = 1;
+	pr = 0;
+	for (int i = 2; i < N; i++) {
+		if (!ox[i]) p[pr++] = i;
+		for (int j = 0;i*p[j]<N&&j < pr; j++)
+			ox[i*p[j]] = 1;
+	}
+}
+
+int main() {
+	PrimeTable();
+	int n;
+	while (cin >> n, n)
+		cout << ox[n] << endl;
+}
