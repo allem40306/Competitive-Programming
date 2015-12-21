448d36ac1aa4494ccf5806af4429b92208ba7482
allem40306 <allem40306@gmail.com>
2015-12-21 20:45:56 +0800

    important.cpp

--- struct/important.cpp
+++ struct/important.cpp
@@ -3,27 +3,40 @@
 #include <string>
 #include <cstdio>
 using namespace std;
-int arr[1010];
-int heapp[600000]; 
+int arr[1010], heapp[600000];
+int bp;
 
-int bulit(int x,int R,int L){
-	if (L - R == 0)return 0;
-	if (L - R == 1)return arr[L];
+int bulit(int L, int R, int x){
+	if (R - L == 0)return 0;
+	if (R - L == 1)return arr[L];
+	int M = (L + R) / 2;
+	heapp[bp++] = bulit(L, M, x) + bulit(M, R, x);
+	if ()
+
+}
+
+int modfiy(int L, int R){
+
+}
+
+int quest(int L, int R){
 
 }
 
 int main() {
 	int n, t = 0;
-	while (scanf("%d",n)){
+	while (scanf("%d", n)){
 		if (t)printf("\n");
 		printf("Case %d", ++t);
 		memset(arr, 0, sizeof(arr));
 		for (int i = 0; i < n; i++)scanf("%d", arr[i]);
-		bulit(1,0,n);
+		int bp = 0;
+		bulit(0, n, 1);
 		string s, x, y;
-		while (scanf("%s",s), s != "END"){
-			if (s == "S");
-			else if (s == "M");
+		while (scanf("%s", s), s != "END"){
+			cin >> x >> y;
+			if (s == "S")modfiy(0, n, x, y);
+			else if (s == "M")quest(0, n, x, y);
 		}
 	}
 }
