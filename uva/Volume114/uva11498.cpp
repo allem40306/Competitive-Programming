f01b73f589bbf98bfccab32574342546016fca28
allem40306 <allem40306@gmail.com>
2016-03-26 10:24:15 +0800

    init commit

---
+++ uva/Volume114/uva11498.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+	int n, a, b, a1, b1;
+	while (cin >> n, n){
+		cin >> a >> b;
+		while (n--){
+			cin >> a1 >> b1;
+			if (a == a1 || b == b1)printf("divisa");
+			else{
+				if (b1 > b)printf("N");
+				else printf("S");
+				if (a1 > a)printf("E");
+				else printf("O");
+			}
+			printf("\n");
+		}
+	}
+}
