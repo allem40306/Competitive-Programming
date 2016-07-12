af24ef2fd5741d706ac14cb48dd386c15f7f4fbd
allem40306 <allem40306@gmail.com>
2016-07-12 20:28:58 +0800

    go go

---
+++ uva/Volume101/uva10165.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+#define N 100
+
+int main() {
+	int n, m, o;
+	while (cin >> n, n){
+		cin >> m;
+		for (int i = 1; i < n; i++){
+			cin >> o;
+			m ^= o;
+		}
+		if (m)printf("Yes\n");
+		else printf("No\n");
+	}
+}
---
+++ uva/Volume113/uva11364.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+#define N 100
+
+int main() {
+	int t, n, a[N];
+	cin >> t;
+	for(int i = 0; i < t; i++){
+		cin >> n;
+		for (int j = 0; j < n; j++)cin >> a[j];
+		sort(a, a + n);
+		printf("%d\n", 2*(a[n-1]-a[0]));
+	}
+}
