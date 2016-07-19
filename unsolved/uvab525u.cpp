6f94164bd9ecb31c3db7fe48cb06428e0297b661
allem40306 <allem40306@gmail.com>
2016-07-19 19:07:51 +0800

    init commit

---
+++ unsolved/uvab525u.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+#define MOD 100000007
+
+int times(int a, int b, int k){
+	int c = a + b;
+	for (int i = 0; i <= k; i++){
+		c = (a + b) % MOD;
+		a = b; b = c;
+	}
+	return c;
+}
+
+int main(){
+	int m, f, k;
+	while (cin >> m >> f >> k){
+		cout << times(m, f, k) % MOD << endl;
+	}
+}
---
+++ uva/Volume101/uva10114.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+#define N 1000
+
+int main(){
+	int n, m;
+	double f, c; //first cost
+	double d[N];
+	while (cin >> n >> f >> c >> m,n >= 0){
+		for (int i = 0; i < N; i++)d[i] = 0.0;
+		int dr = 0;
+		while (m--){
+			cin >> dr;
+			cin >> d[dr];
+		}
+		double v = f + c, o = c, e = c / n; //vaule owes earn
+		for (int i = 0; i <= n; i++){
+			d[i] = (d[i]>0 ? d[i] : d[i - 1]);
+			v *= 1 - d[i];
+			if (v >= o){
+				printf("%d month%s\n", i,i != 1 ? "s" : "");
+				break;
+			}
+			o -= e;
+		}
+	}
+}
