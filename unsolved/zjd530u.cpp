c17de219d803d5c7052dff9efc04b5a16bf0c1ac
allem40306 <allem40306@gmail.com>
2016-07-20 19:55:41 +0800

    o.o

---
+++ unsolved/zjd530u.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+#define N 3000
+short a[N][N], b[N][N], c, n;
+
+int sum(int x, int y){
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+		sum += a[x][i] * b[i][y];
+	return sum;
+}
+
+int main(){
+	while (cin >> n){
+		bool flag = 1;
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				cin >> a[i][j];
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				cin >> b[i][j];
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++){
+			cin >> c;
+			if (flag){ if (sum(i, j) % 2 != c)flag = 0; }
+			}
+		printf(flag ? "YES\n" : "NO\n");
+	}
+}
---
+++ zerojudge/zjb528.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+const double pi = acos(-1.0);
+#define m(a) a*a
+
+int main(){
+	double f;
+	while (cin >> f){
+		f = acos(asin(f));
+		f = f * 180 / pi;
+		if(f>=0.0)cout << fixed << setprecision(2) << f << endl;
+		else printf("FAIL!\n");
+	}
+}
