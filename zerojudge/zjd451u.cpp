60181241ee530c89d2cb4d99caa9d7b583ae3c6e
allem40306 <allem40306@gmail.com>
2016-07-27 20:04:17 +0800

    fighting

---
+++ zerojudge/zjd451u.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main() {
+	int n, m, i = 0;
+	for (cin >> n; i < n; i++){
+		string s, r;
+		cin >> s >> m;
+		int lens = s.size(), p = 0, pp = 0, mu = 0;
+		for (int j = 0; j < lens; j++){
+			if (s[j] == ','){ mu++; continue; }
+			if (p!=1&&s[j] == '.')p = -1;
+			else {
+				if (p != 1&&s[j]!='0'){
+					p = 1;
+					if (!pp&&s[j]!='0')pp = j;
+				}
+				r += s[j];
+			}
+		}
+		int lenr = r.size();
+		if (p == 1){
+			int i = 1; 
+			cout << r[i - 1] << '.';
+			for (; i < m - 1; i++)cout << r[i];
+			if (i + 1 < lenr)cout << int(r[i]-'0' + (r[i + 1] >= '5'));
+			printf("(%d)\n", lenr - 1);
+		}
+	}
+}
---
+++ zerojudge/zjd452.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n, m, i = 0;
+	for (cin >> n; i < n; i++){
+		int a[200] = {};
+		cin >> m;
+		for (int j = 0; j < m; j++)cin >> a[j];
+		sort(a, a + m);
+		int ans = 0;
+		for (int k = 0; k < m; k++)
+			ans += (abs(a[m/2] - a[k]));
+		printf("%d\n", ans);
+	}
+}
