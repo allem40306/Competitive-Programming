09d1bd6158d292e1b1b51607525453bbad9a3ee7
allem40306 <allem40306@gmail.com>
2016-08-01 19:46:01 +0800

    acac

---
+++ zerojudge/zjd555.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+#define N 600000
+
+struct xy{
+	int x, y;
+	bool b = 1;
+}a[N];
+
+bool cmp(xy a, xy b){
+	if (a.y != b.y)
+		return a.y > b.y;
+	return a.x > b.x;
+}
+
+int main() {
+	int n, nt = 0, i;
+	while (cin >> n){
+		for (i = 0; i < n; i++){
+			cin >> a[i].x >> a[i].y;
+			a[i].b = 0;
+		}
+		sort(a, a + n, cmp);
+		int nn = 1;
+		xy k = a[0]; a[0].b = 1;
+		for (i = 1; i < n; i++){
+			if (a[i].y != k.y&&a[i].x > k.x){
+				k = a[i];
+				a[i].b = 1;
+				nn++;
+			}
+		}
+		printf("Case %d: \nDominate Point: %d\n", ++nt, nn);
+		for (int i = 0; i < n; i++)
+			if(a[i].b)printf("(%d,%d)\n", a[i].x, a[i].y);
+	}
+}
---
+++ zerojudge/zjd625.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+#define N 150
+#define M 8
+int dx[M] = { -1, -1, -1, 0, 0, 1, 1, 1 }, dy[M] = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+int main() {
+	string s[N];
+	int n, a[N][N], i, j;
+	while (cin >> n){
+		for (i = 0; i < n; i++)cin >> s[i];
+		for (i = 0; i < n; i++)
+			for (j = 0; j < n; j++)
+				a[i][j] = 0;
+		for (i = 0; i < n; i++){
+			for (int j = 0; j < n; j++){
+				if (s[i][j] == '*'){
+					for (int k = 0; k < M; k++){
+						int x = i + dx[k], y = j + dy[k];
+						if (x >= 0 && x < n&&y >= 0 && y < n)a[x][y]++;
+					}
+				}
+			}
+		}
+		for (i = 0; i < n; i++){
+			for (int j = 0; j < n; j++){
+				if (s[i][j] == '*')printf("*");
+				else if (a[i][j])printf("%d", a[i][j]);
+				else printf("-");
+			}
+			printf("\n");
+		}
+	}
+}
