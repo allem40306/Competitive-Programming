dd8403b996e37dcf6c7e9235ed7519eeab1d06a7
allem40306 <allem40306@gmail.com>
2016-08-06 09:19:01 +0800

    fighting

---
+++ unsolved/zjd119u.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+#define N 10
+#define M 50000
+
+int main(){
+	unsigned int i, j;
+	unsigned int coin[N] = { 1, 5, 10, 20, 50, 100, 200, 500, 1000, 2000 };
+	unsigned long long int poss[M] = { 1 };
+	for (i = 0; i < N; i++)
+		for (j = coin[i]; j < M; j++)
+			poss[j] += poss[j - coin[i]];
+	string s;
+	while (getline(cin, s), s != "0"){
+		int sum = 0, sum2;
+		istringstream is(s);
+		while (is >> sum2){
+			sum += sum2;
+		}
+		cout << poss[sum] - 1 << endl;
+	}
+}
---
+++ zerojudge/zjd561.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string s;
+	while (cin >> s){
+		int i = 0, j = 0;
+		while (1){
+			if (s[i] == '.')break;
+			i++;
+		}
+		while (s.size() <= i + 3)s += '0';
+		if (s[i+3] >= '5')s[i+2]++;
+		if (s[i+2] > '9')s[i+1]++, s[i+2] = '0';
+		if (s[i + 1] > '9')s[i - 1]++, s[i + 1] = '0';
+		s = s.substr(0, i + 3);
+		if (s == "-0.00") s = "0.00";
+		cout << s<< endl;
+	}
+}
