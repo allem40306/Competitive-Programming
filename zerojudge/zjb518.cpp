13b916987d18249fc4275ef196ec93338b952951
allem40306 <allem40306@gmail.com>
2016-08-10 20:06:34 +0800

    ac*2

---
+++ zerojudge/zjb518.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+#define N 80
+
+struct fe{
+	int f, e;
+}p[N];
+
+void init(){
+	for (int i = 0; i < N; i++)
+		p[i].f = p[i].e = -1;
+	return;
+}
+
+int main(){
+	int n, ni = 0, m, i, j;
+	for (cin >> n; ni < n; ni++){
+		init();
+		char ch;
+		cin >> m;
+		for (int mi = 0; mi < m; mi++){
+			cin >> i >>ch>> j;
+			if (j == 99){ p[i].f = -2; }
+			else{ p[i].f = j; p[j].e = i; }
+		}
+		if (ni)printf("\n");
+		for (i = 0; i < m; i++){
+			if (p[i].e == -1){
+				int ii = i, ans[N], ansr = 0;
+				while (p[ii].f!=-2&&p[p[ii].f].f != -2){
+					ans[ansr++] = p[ii].f;
+					ii = p[ii].f;
+				}
+				printf("%d", i);
+				if (!ansr)printf(":N\n");
+				else{
+					printf(":{");
+					for (int ai = 0; ai < ansr; ai++)
+						printf("%s%d", ai ? "," : "", ans[ai]);
+					printf("}\n");
+				}
+			}
+		}
+	}
+}
---
+++ zerojudge/zjb520.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+	int n, i = 0;
+	for (cin >> n; i < n; i++){
+		int a[5], b[5], ans = 0;
+		char ch;
+		cin >> a[0] >> ch >> a[1] >> ch >> a[2] >> ch >> a[3] >> ch >> a[4];
+		cin >> b[0] >> ch >> b[1] >> ch >> b[2] >> ch >> b[3] >> ch >> b[4];
+		for (int i = 0; i < 5; i++)
+			for (int j = 0; j < 5; j++)
+				ans += a[i] == b[j];
+		printf("%d\n", ans);
+	}
+}
