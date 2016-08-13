9dbadfeae4cdbd440e10721d1d6b4465d1569390
allem40306 <allem40306@gmail.com>
2016-08-13 12:06:57 +0800

    ac*1

--- unsolved/zja813u.cpp
+++ unsolved/zja813u.cpp
@@ -1,34 +1,23 @@
 ﻿#include <iostream>
 #include <algorithm>
 using namespace std;
-#define N 1000000
-unsigned long long lis[N][2], h[N], n, ans;
-
-void init(){
-	for (int i = 0; i < n; i++)
-		lis[i][0]=lis[i][1] = 0;
-	ans = 0;
-	return;
-}
+#define N 10000 //1000000
+#define max(x,y) x>y?x:y
 
 int main(){
-	int n;
+	int h[N], top[N], see[N], n, tr = 0;
+	long long ans = 0;
 	cin >> n;
-	init();
-	for (int i = 0; i < n; i++)cin >> h[i];
-	for (int j = 0; j < n; j++)
-		for (int k = j+1; k < n; k++){
-		if (h[j] <= h[k]){
-			lis[k][0] = (lis[k][0] > lis[j][0] + 1 ? lis[k][0] : lis[j][0] + 1);
-			}
+	for (int i = 0; i < n; i++){cin >> h[i]; see[i] = 0;}
+	top[0] = 1;
+	for (int j = 1; j < n; j++){
+		if (h[j]>h[top[tr]]){
+			see[top[tr]] = j - top[tr];
+			top[++tr] = j;
 		}
-	for (int j = n-1; j>=0; j--)
-		for (int k = j - 1; k >= 0; k--){
-		if (h[j] <= h[k]){
-			lis[k][1] = (lis[k][1] > lis[j][1] + 1 ? lis[k][1] : lis[j][1] + 1);
-			}
-		}
-	for (int i = 0; i < n; i++)
-		ans += lis[i][0] + lis[i][1];
-	cout << ans << endl;
+	}
+	for (int i = 0; i < n; i++){
+		ans += see[i];
+	}
+	cout << ans*2 << endl;
 }
---
+++ zerojudge/zja273.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+	int n, k;
+	while (cin >> n >> k){
+		if (!n)printf("Ok!\n");
+		else if (!k)printf("Impossib1e!\n");
+		else printf("%s\n", n%k?  "Impossib1e!":"Ok!" );
+	}
+}
