006a201ef2de83544b353ed15f0aa0484f2c062f
allem40306 <allem40306@gmail.com>
2016-08-14 08:52:10 +0800

    ac*1

---
+++ zerojudge/zja818.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+#define N 30
+
+int main(){
+	int n, k, arr[N], ii;
+	string s, t;
+	while (cin >> n){
+		for (int i = 0; i < n; i++){
+			cin >> ii;
+			arr[ii - 1] = i;
+	}
+		cin >> s >> k;
+		for (int j = 0; j < k; j++){
+			t = s;
+			for (int i2 = 0; i2 < n; i2++){
+				s[arr[i2]] = t[i2];
+			}
+		}
+		cout << s << endl;
+	}
+}
