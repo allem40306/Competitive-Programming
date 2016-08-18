c7f3c793831638fd81ad203ff3825ab4abc0be17
allem40306 <allem40306@gmail.com>
2016-08-18 16:41:04 +0800

    ac*3

---
+++ zerojudge/zja536.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+	int n, e, f, c;
+	cin >> n;
+	while (n--){
+		int ans = 0;
+		cin >> e >> f >> c;
+		e += f;
+		while (e / c){
+			ans += e / c;
+			e = e / c + e%c;
+		}
+		printf("%d\n", ans);
+	}
+}
---
+++ zerojudge/zjd289.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+#define N 8
+#define M 8010
+
+int main(){
+	int i, j, n;
+	unsigned int coin[N] = { 1, 13, 33, 43, 139, 169,1309, 2597 };
+	unsigned long long int poss[M] = { 1 };
+	for (i = 0; i < N; i++)
+		for (j = coin[i]; j < M; j++){
+			poss[j] += poss[j - coin[i]];
+		}
+	while(cin >> n){
+		cout << poss[n] << endl;
+	}
+}
