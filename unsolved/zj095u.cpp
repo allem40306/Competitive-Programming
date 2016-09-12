3b874badf56fdad43319129a688df1431ba772a0
allem40306 <allem40306@gmail.com>
2016-09-12 21:32:32 +0800

    set

---
+++ unsolved/zj095u.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include<set>
+using namespace std;
+#define N 100
+int main() {
+	int m, n, k, mi = 0, p;
+	char ch;
+	for (cin >> m; mi < m; mi++){
+		int a[N], b[N];
+		set <int> w;
+		set<int>::iterator it;
+		cin >> n >> k;
+		for (int ni = 1; ni <= n; ni++)
+			w.insert(ni);
+		for (int ki = 0; ki < k; ki++){
+			cin >> p;
+			for (int pi = 0; pi < p; pi++)
+				cin >> a[pi] >> b[pi];
+			cin >> ch;
+			if (ch == '=')
+				for (int pi = 0; pi < p; pi++){
+				it = w.find(a[pi]);
+				if(*it<0)w.erase(it);
+				it = w.find(b[pi]);
+				if(*it<0)w.erase(it);
+				}
+		}
+		if (w.size()>1)printf("0\n");
+		else {
+			it = w.begin();
+			cout << *it << endl;
+		}
+	}
+}
---
+++ uva/Volume120/uva12096.cpp
@@ -0,0 +1,46 @@
+﻿#include<iostream>
+#include<string>
+#include<set>
+#include<map>
+#include<stack>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+#define ALL(x) x.begin(),x.end()
+#define INS(x) inserter(x,x.begin())
+
+typedef set<int> Set;
+map<Set, int> IDcache; // 把集合映射成ID
+vector<Set> Setcache; // 根据ID取集合
+
+// 查找给定集合x的ID。如果找不到，分配一个新ID
+int ID(Set x) {
+	if (IDcache.count(x)) return IDcache[x];
+	Setcache.push_back(x); // 添加新集合
+	return IDcache[x] = Setcache.size() - 1;
+}
+
+int main() {
+	int t, n, ti = 0;
+	string com;
+	for (cin >> t; ti < t; ti++){
+		stack<int>st;
+		for (cin >> n; n; n--){
+			cin >> com;
+			if (com[0] == 'P')st.push(ID(Set()));
+			else if (com[0] == 'D')st.push(st.top());
+			else{
+				Set x1 = Setcache[st.top()]; st.pop();
+				Set x2 = Setcache[st.top()]; st.pop();
+				Set x;
+				if (com[0] == 'U') set_union (ALL(x1), ALL(x2), INS(x));
+				if (com[0] == 'I') set_intersection (ALL(x1), ALL(x2), INS(x));
+				if (com[0] == 'A'){ x = x2; x.insert(ID(x1)); }
+				st.push(ID(x));
+			}
+			cout << Setcache[st.top()].size() << endl;
+		}
+		printf("***\n");
+	}
+}
