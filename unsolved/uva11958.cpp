b4efcf6d897e44ca023f3824590392239b12d93e
allem40306 <allem40306@gmail.com>
2016-09-25 16:30:13 +0800

    unun

---
+++ unsolved/uva11958.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>   
+using namespace std;
+
+int main(){
+	int t, ti = 1;
+	for (cin >> t; ti <= t; ti++){
+		int k, h, m, ans = 10000, h1, m1, mm;
+		char ch;
+		cin >> k>> h >> ch >> m ;
+		for (int i = 0; i < k; i++){
+			cin >> h1 >> ch >> m1 >> mm;
+			int sum = h1 * 60 + m1 - (h * 60 + m);
+			sum = (sum>0 ? sum : sum + 1440) + mm;
+			if(sum < ans)ans = sum;
+		}
+		printf("Case %d: %d\n", ti, ans);
+	}
+}
