7ffc15d05fbd041a725f18a3b1cc44fd9d11a4a2
allem40306 <allem40306@gmail.com>
2017-10-13 16:20:06 +0800

    rename struct -> codebook and other little things

--- codebook/eightqueen.cpp
+++ codebook/eightqueen.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <iomanip>
 using namespace std;
 #define N 100
--- codebook/seg(array).cpp
+++ codebook/seg(array).cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cstring>
 #include <string>
 #include <cstdio>
